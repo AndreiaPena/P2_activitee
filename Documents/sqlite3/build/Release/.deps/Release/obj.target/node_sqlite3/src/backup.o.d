319bc423a10933ba0a18da4812b6ac087133ad3c
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-03-24 17:21:40 +0100

    remove du dossier express vers celui-ci avec les exercices faits

---
+++ Documents/sqlite3/build/Release/.deps/Release/obj.target/node_sqlite3/src/backup.o.d
@@ -0,0 +1,66 @@
+cmd_Release/obj.target/node_sqlite3/src/backup.o := g++ '-DNODE_GYP_MODULE_NAME=node_sqlite3' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DSQLITE_THREADSAFE=1' '-DHAVE_USLEEP=1' '-DSQLITE_ENABLE_FTS3' '-DSQLITE_ENABLE_FTS4' '-DSQLITE_ENABLE_FTS5' '-DSQLITE_ENABLE_JSON1' '-DSQLITE_ENABLE_RTREE' '-DBUILDING_NODE_EXTENSION' '-DNDEBUG' -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/src -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/deps/openssl/config -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/deps/openssl/openssl/include -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/deps/uv/include -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/deps/zlib -I/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/deps/v8/include -I../../nan -I./Release/obj/gen/sqlite-autoconf-3300100  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/node_sqlite3/src/backup.o.d.raw   -c -o Release/obj.target/node_sqlite3/src/backup.o ../src/backup.cc
+Release/obj.target/node_sqlite3/src/backup.o: ../src/backup.cc \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-internal.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-version.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8config.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-platform.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_version.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_buffer.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_version.h \
+ ../src/macros.h ../src/database.h \
+ Release/obj/gen/sqlite-autoconf-3300100/sqlite3.h ../../nan/nan.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/errno.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/version.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/unix.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/threadpool.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/linux.h \
+ /home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h ../../nan/nan_private.h \
+ ../../nan/nan_typedarray_contents.h ../../nan/nan_json.h ../src/async.h \
+ ../src/threading.h ../src/backup.h
+../src/backup.cc:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-internal.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-version.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8config.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/v8-platform.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_version.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_buffer.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_version.h:
+../src/macros.h:
+../src/database.h:
+Release/obj/gen/sqlite-autoconf-3300100/sqlite3.h:
+../../nan/nan.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/errno.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/version.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/unix.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/threadpool.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/uv/linux.h:
+/home/simplon/.node-gyp/13.0.0-nightly20190802452b393c1f/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_private.h:
+../../nan/nan_typedarray_contents.h:
+../../nan/nan_json.h:
+../src/async.h:
+../src/threading.h:
+../src/backup.h:
